847b4331cc75fea4291b998dc6dab9c90a55eac7
Fitsum A <fitsumcs@gmail.com>
2021-04-06 05:16:00 +0400

    display information about the data

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sklearn import datasets
 st.title("Streamlit Based App")
 st.write(""" 
 
-   This is a Project the allow you to compare Maching learing algorthims !!!
+   This is a Project the allow you to compare Machine learing algorthims !!!
 
 
 """)
@@ -13,9 +13,6 @@ st.write("""
 
 dataset_choice =  st.sidebar.selectbox("Dataset Selection", ("Breast Cancer", "Wine Dataset", "Iris"))
 algorithm_choice =  st.sidebar.selectbox("Algorithm Selection", ("SVM", "KNN", "RF"))
-
-st.write(dataset_choice)
-st.write(algorithm_choice)
 
 
 
@@ -26,5 +23,12 @@ def get_data(dataset_choice):
         data = datasets.load_breast_cancer()
     else:
         data = datasets.load_iris()
+    X = data.data
+    y = data.target
+    return X,y
+
+# Display the info about the data 
+X,y = get_data(dataset_choice)
+st.write("Shape : " , X.shape)
 
 
